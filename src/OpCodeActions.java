827491129958bf0007415f548d1401f8b9ce36d0
Matthew Hughes <me@matthewhughes.co.uk>
2013-04-11 13:42:52 +0100

    Finished OpCodes

--- src/OpCodeActions.java
+++ src/OpCodeActions.java
@@ -9,14 +9,61 @@ public class OpCodeActions {
 
     Registers Reg = new Registers(); // from The Bill
     public static void StoreDirect(int RegAdd){
+        TestRegBounds(RegAdd);
+        Registers Reg = new Registers(); // from The Bill
+        int Accumulator = Reg.GetAcc();
+        Reg.SetReg(Accumulator, RegAdd);
+    }
+    public static void  LoadDirect(int RegAdd){
+       TestRegBounds(RegAdd);
+       Registers Reg = new Registers();
+       int Value = Reg.GetReg(RegAdd);
+       Reg.SetAcc(Value);
+    }
+    public static void LoadImmediate(int Value){
+        Registers Reg = new Registers();
+        Reg.SetAcc(Value);
+    }
+
+    public static void Stop(){
+        System.out.println("Stopping now");
+        System.exit(1);
+    }
+
+    public static void Add(int RegAdd){
+        TestRegBounds(RegAdd);
+        Registers Reg = new Registers();
+        int RegValue = Reg.GetReg(RegAdd);
+        int Total = Reg.GetAcc() + RegValue;
+        Reg.SetAcc(Total);
+    }
+    public static void Sub(int RegAdd){
+        TestRegBounds(RegAdd);
+        Registers Reg = new Registers();
+        int RegValue = Reg.GetReg(RegAdd);
+        int Total = Reg.GetAcc() - RegValue;
+        Reg.SetAcc(Total);
+    }
+    public static void Mul(int RegAdd){
+        TestRegBounds(RegAdd);
+        Registers Reg = new Registers();
+        int RegValue = Reg.GetReg(RegAdd);
+        int Total = Reg.GetAcc() * RegValue;
+        Reg.SetAcc(Total);
+    }
+
+    public static void Sqr(int RegAdd){
+        TestRegBounds(RegAdd);
+        Registers Reg = new Registers();
+        int RegValue = Reg.GetReg(RegAdd);
+        int Total = Reg.GetAcc() * Reg.GetAcc();
+        Reg.SetAcc(Total);
+    }
+    public static void TestRegBounds(int RegAdd){
         if(RegAdd > 24){
             System.out.println("Register Address Out Of Bounds");
             System.out.println("Catastrophic failure");
             System.exit(1);
         }
-        Registers Reg = new Registers(); // from The Bill
-        int Accumulator = Reg.GetAcc();
-        Reg.SetReg(Accumulator, RegAdd);
     }
-
 }
